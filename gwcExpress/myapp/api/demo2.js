e25e244aef632106470fc32c7771693f7a5745f9
guanweicheng <1396539283@qq.com>
2018-06-08 15:49:26 +0800

    源码上传

---
+++ gwcExpress/myapp/api/demo2.js
@@ -0,0 +1,32 @@
+// api/demo2.js
+
+var dataDemo={
+    'code':'000',
+    'message':'demo2',
+    'lists':[
+        {
+            'name':'小明',
+            'age': '12',
+            'sex':'男'
+        },{
+            'name':'小红',
+            'age': '12',
+            'sex':'女'
+        }
+    ]
+}
+exports.getData = function(method,data){
+    var backData={
+        "code":'000',
+        "msg":"",
+        "data":dataDemo
+    }
+    if(method=='DELETE'){
+        backData={
+            "code":'999',
+            "msg":"不支持DELETE方法"
+        }
+    }
+    return JSON.stringify(backData);
+}
+
